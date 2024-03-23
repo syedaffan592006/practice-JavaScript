641360c2f2df374c0c13176ec0d1271e07ce22a7
Syed Affan Asif <154745160+syedaffan592006@users.noreply.github.com>
2024-03-23 09:19:59 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
-// pdf 1
-alert ("syed affan asif");
-// pdf2
-let name  ;
-let tree1;
-let t2ree;
-// pdf3
-let myNumber = 1;
-let age = 17;
-let marks= 78;
+alert(message);
+
+/* Question 4 */
+let student_name = prompt("Enter Your Name");
+let student_age = prompt("Enter Your Age");
+/* Question 1 */
+let username;
+
+/* Question 2 */
+let myname = "Sufyan Sami";
+
+/* Question 3 */
+let message = "Hello World";
+let student_course = prompt("Enter Your Course");
+alert(
+  "Student Name: " +
+    student_name +
+    "\n" +
+    "Student Age: " +
+    student_age +
+    "\n" +
+    "Student Course: " +
+    student_course
+);
+
+/* Question 5 */
+alert("PIZZA" + "\n" + "PIZZ" + "\n" + "PIZ" + "\n" + "PI" + "\n" + "P");
+
+/* Question 6 */
+let email = "example@gmail.com";
+alert("Email: " + email);
+
+/* Question 7 */
+let book =
+  "I'm currently reading a book";
+alert(book);
+
+/* Question 8 */
+document.write("Yah! I can write HTML content through Javascript");
+
+/* Question 9 */
+let lines = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(lines);
 
 
