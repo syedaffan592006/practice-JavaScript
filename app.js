dcb140786e8e5527164252103662890935318d09
Syed Affan Asif <154745160+syedaffan592006@users.noreply.github.com>
2024-03-23 14:14:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 /* Question 1 */
 let username;
-alert(message);
 
 /* Question 2 */
 let myname = "Sufyan Sami";
 
 /* Question 3 */
 let message = "Hello World";
+alert(message);
+
+/* Question 4 */
+let student_name = prompt("Enter Your Name");
+let student_age = prompt("Enter Your Age");
 let student_course = prompt("Enter Your Course");
 alert(
   "Student Name: " +
@@ -18,10 +22,6 @@ alert(
     "Student Course: " +
     student_course
 );
-
-/* Question 4 */
-let student_name = prompt("Enter Your Name");
-let student_age = prompt("Enter Your Age");
 
 /* Question 5 */
 alert("PIZZA" + "\n" + "PIZZ" + "\n" + "PIZ" + "\n" + "PI" + "\n" + "P");
@@ -41,5 +41,4 @@ document.write("Yah! I can write HTML content through Javascript");
 /* Question 9 */
 let lines = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
 alert(lines);
-
 
