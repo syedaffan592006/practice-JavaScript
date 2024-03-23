65c1b05c6cf1fed3925c46021de6983826fba33f
syedaffan592006 <syedaffan592006@gmail.com>
2024-03-23 15:20:01 -0700

    break

---
+++ js assignment ch 04/appp.js
@@ -0,0 +1,32 @@
+// Q:1
+let x = 1, y = 2, z = 3;
+
+// Q:2
+// legall variable
+let Geeks;
+let Geeks_for_Geeks;
+let $Geeks;
+let _Geeks;
+let Geeks123;
+
+// Q:2
+// Ilegall variable
+// let Geeks-for-Geeks;  
+// let 123Geeks;  
+// let Geeks for Geeks;  
+
+// Q3:
+// (A)
+document.write("Rules for namung JS variable")
+
+
+
+
+
+
+
+
+
+
+
+
